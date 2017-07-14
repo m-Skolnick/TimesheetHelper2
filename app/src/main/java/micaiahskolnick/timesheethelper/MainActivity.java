590395cd83cbbadb7d2c71c25633f0cba5d94253
Micaiah Skolnick <micaiahskolnickdeveloper@gmail.com>
2017-07-14 00:03:13 -0500

    First commit after copying and re-writing parts of code to make app work again on google play.

--- app/src/main/java/micaiahskolnick/timesheethelper/MainActivity.java
+++ app/src/main/java/micaiahskolnick/timesheethelper/MainActivity.java
@@ -1,18 +1,30 @@
 package micaiahskolnick.timesheethelper;
 
 import android.content.SharedPreferences;
+
+
+import java.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.Time;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import micaiahskolnick.timesheethelper.R;
-
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    To do list
+
+    *Set to display doubele "0" when an exact hour is hit
+
+
+
+     */
 
     private SharedPreferences preferenceSettings;
     private SharedPreferences.Editor preferenceEditor;
@@ -22,10 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     private EditText goalBox;
-    private EditText goal2Box;
     private EditText currBox;
-    private EditText clockInTimeBox;
-    private TextView testSaveName;
+    private EditText clockInTimeHr;
+    private EditText clockInTimeMin;
+    private EditText clockOutTime;
     private TextView timeNeededHoursBox;
     private TextView timeNeededMinutesBox;
     private TextView timeNeededHours2Box;
@@ -34,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView hoursHeading;
     private TextView minutesHeading;
     private LinearLayout resultsLayout;
+    private Button AmPmBtn;
+    private Calendar calendar;
+
 
 
     private double goal;
@@ -42,21 +57,115 @@ public class MainActivity extends AppCompatActivity {
     private double needed;
     private double neededHours;
     private double neededMinutes;
+    private double clockInTimeMinDouble;
+    private double clockInTimeHrDouble;
     private int neededHoursInt;
     private int neededMinInt;
     private String goalStr;
     private String timeNeededHoursStr;
     private String timeNeededMinutesStr;
+    String clockOutTimeStr;
 
     public void updateDisplay(){
         getNumbers();
-        calculate();
-        setText();
-
-        //calculate();
+        calculateTimeNeeded();
+        setTimeNeededText();
+        showOrHideResults();
+        updateClockOutTime();
+    }
+    private void updateClockOutTime(){
+        if(getClockOutTime()) {
+            calculateClockOutTime();
+            updateClockOutDisplay();
+        }
 
     }
+    private boolean getClockOutTime(){
+        try {
+            clockInTimeHrDouble = Double.parseDouble(clockInTimeHr.getText().toString());
+        }
+        catch (Exception e){
+            clockInTimeHrDouble = 0;
+            return false;
+        }
+        try {
+            clockInTimeMinDouble = Double.parseDouble(clockInTimeMin.getText().toString());
+        }
+        catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+    private void calculateClockOutTime(){
 
+
+        int clockOutTimeHrInt;
+        int clockOutTimeMinInt;
+        boolean PM = true;
+        if(AmPmBtn.isActivated()){
+            PM = false;
+        }
+
+        clockOutTimeHrInt = (int) clockInTimeHrDouble + neededHoursInt;
+        clockOutTimeMinInt = (int) clockInTimeMinDouble + neededMinInt;
+
+        while(clockOutTimeMinInt > 60){
+            clockOutTimeHrInt++;
+            clockOutTimeMinInt -=60;
+        }
+        while(clockOutTimeHrInt > 12){
+            clockOutTimeHrInt -=12;
+            PM = !PM;
+            if(clockOutTimeHrInt == 12){
+                PM = !PM;
+            }
+        }
+
+        clockOutTimeStr = Integer.toString(clockOutTimeHrInt);
+
+
+            //Concatenate all of the pieces of clock out time together to one string.
+        clockOutTimeStr = clockOutTimeStr.concat(":").concat(Integer.toString(clockOutTimeMinInt));
+        if(PM){
+            clockOutTimeStr = clockOutTimeStr.concat(" PM");
+        }
+        else{
+            clockOutTimeStr = clockOutTimeStr.concat(" AM");
+        }
+
+
+    }
+    private void updateClockOutDisplay(){
+        clockOutTime.setText(clockOutTimeStr);
+    }
+    private void setCurrentTimeAsClockInTime(){
+        int currentHour,currentMinute;
+        Calendar.getInstance();
+
+        Time currentTime = new Time();
+        currentTime.setToNow();
+        currentHour = currentTime.hour;
+        if(currentHour > 12){
+            currentHour -=12;
+            AmPmBtn.setSelected(false);
+        }
+        else{
+            AmPmBtn.setSelected(true);
+        }
+        currentMinute = currentTime.minute;
+        clockInTimeHr.setText(Integer.toString(currentHour));
+        clockInTimeMin.setText(Integer.toString(currentMinute));
+    }
+    private void showOrHideResults(){
+        try {
+            current = Double.parseDouble(currBox.getText().toString());
+            resultsLayout.setVisibility(View.VISIBLE);
+        }
+        catch (Exception e){
+            current = 0;
+            resultsLayout.setVisibility(View.INVISIBLE);
+        }
+    }
     public void getNumbers(){
 
         try {
@@ -73,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void calculate(){
+    public void calculateTimeNeeded(){
         needed = goal - current;
         neededMinutes = needed*60;
         neededHours = (int) needed;
@@ -81,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
         neededHoursInt = (int) neededHours;
         neededMinInt = (int) neededMinutes;
     }
-    public void setText(){
+    public void setTimeNeededText(){
 
         timeNeededHoursStr = Integer.toString(neededHoursInt);
         timeNeededMinutesStr = Integer.toString(neededMinInt);
@@ -127,6 +236,8 @@ public class MainActivity extends AppCompatActivity {
             goalBox.setText("40");
         }
         currBox.requestFocus(); //Put the focus on the box for current total
+        setCurrentTimeAsClockInTime();
+
     }
 
     @Override
@@ -143,6 +254,10 @@ public class MainActivity extends AppCompatActivity {
 
         timeNeededHoursBox = (TextView) findViewById(R.id.time_needed_hours);
         timeNeededMinutesBox = (TextView) findViewById(R.id.time_needed_minutes);
+        clockInTimeHr = (EditText) findViewById(R.id.clock_in_time_hr);
+        clockInTimeMin = (EditText) findViewById(R.id.clock_in_time_min);
+        clockOutTime = (EditText) findViewById(R.id.clock_out_time);
+        AmPmBtn = (Button) findViewById(R.id.am_pm_btn);
         resultsLayout = (LinearLayout) findViewById(R.id.results_layout);
 
         currBox.setOnClickListener(new View.OnClickListener() {
@@ -159,7 +274,19 @@ public class MainActivity extends AppCompatActivity {
                 updateDisplay();
             }
         });
-
+        clockInTimeHr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearBox(clockInTimeHr);
+                // Need to update the clock out time, but haven't written function yet
+            }
+        });
+        clockInTimeMin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearBox(clockInTimeMin);
+            }
+        });
         // Add a listener to update values when text in input box is changed.
         currBox.addTextChangedListener(new TextWatcher() {
             @Override
@@ -195,6 +322,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        clockInTimeMin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
+
+                updateClockOutTime();
+            }
+
+            @Override
+            public void afterTextChanged(final Editable s) {
+
+            }
+        });
+        clockInTimeHr.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
+
+                updateClockOutTime();
+            }
+
+            @Override
+            public void afterTextChanged(final Editable s) {
+
+            }
+        });
+
 
     }
 }
